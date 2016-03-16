cd89d4f45ceb7c91490d1a156d1d41f819844087
avinashkellucian <avinash.kaul@ellucian.com>
2016-03-16 14:13:23 -0500

    Updates

---
+++ HEDMAdapter/HEDMFileAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEDMAdapter
+{
+    public class HEDMFileAdapter
+    {
+
+        private string _dataFolder;
+
+        public HEDMFileAdapter( string dataFolder)
+        {
+            this._dataFolder = dataFolder;
+        }
+
+
+
+    }
+}
